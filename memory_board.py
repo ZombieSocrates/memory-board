639e379e114f57f501843d30acfb26727a3f634e
Ross Weijer <ross.t.weijer@gmail.com>
2017-02-25 16:55:16 -0600

    Choose random words for cards in memory_board.py

--- memory_board.py
+++ memory_board.py
@@ -50,10 +50,29 @@ class MemoryBoard(object):
     def board(self, board):
 	board_vals = np.repeat('X', repeats=2 * self.n_pairs)
 	self.__board = np.mat(board_vals).reshape((self.rows, self.columns))
- 
+
+    @property
+    def pair_names(self):
+	return self.__pair_names
+
+    @pair_names.setter
+    def pair_names(self, pair_names):
+	# Version of numpy I was originally using came out
+	# before np.random.choice was an option. the while loop
+	# ensures that we sample without replacement
+	random_indices = np.sort(np.random.randint(0, len(words),\
+					   size = self.n_pairs))
+	unique_test = np.sort(np.unique(random_indices))
+	while np.array_equal(random_indices,unique_test) is False:
+	    random_indices = np.sort(np.random.randint(0, len(words),\
+					   size = self.n_pairs))
+	    unique_test = np.sort(np.unique(random_indices))        
+	self.__pair_names = [words[i] for i in random_indices]
+
+#Just for debugging 
 if __name__ == '__main__':
     test1 = MemoryBoard(10)
     print(test1.rows)
     print(test1.columns)
     print(test1.board) 
-	
+    print(test1.pair_names)	
