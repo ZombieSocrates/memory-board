6bbdc2e51fba68958bd2e1ccc025e5eb9c420fe3
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-03-03 17:34:51 -0600

    Removed needless setters from property rows and columns as test

--- memory_board.py
+++ memory_board.py
@@ -36,75 +36,55 @@ class MemoryBoard(object):
 		Will be used to supply the correct object for any card that a user "flips over."
 
 	'''
-	def __init__(self, n_pairs, rows= 0, columns= 0, board = None,
-				 pair_names = None, solution_dict = None):
+	def __init__(self, n_pairs):
 		self.n_pairs = n_pairs
-		self.columns = columns 
-		self.rows = rows
-		self.board = board
-		self.pair_names = pair_names
-		self.solution_dict = solution_dict
-
-	@property
-	def columns(self):
-		return self._columns
 
 	# columns = the largest integer <= sqrt(total number of cards in play) that is also
 	# a factor of total number of cards in play
-	@columns.setter
-	def columns(self,columns):
+	@property
+	def columns(self):
 		area = 2 * self.n_pairs
 		best_guess = floor(np.sqrt(area))
 		while area % best_guess != 0:
 			best_guess -= 1
-		self._columns = int(best_guess)
+		return int(best_guess)
 
+	# rows = total cards/columns derived above	
 	@property
 	def rows(self):
-		return self._rows
-
-	# rows = total cards/columns 
-	@rows.setter
-	def rows(self, rows):
-		self._rows = int((2 * self.n_pairs)/self.columns)
-   
-	@property
-	def board(self):
-		return self._board
+		return int((2 * self.n_pairs)/self.columns)
 
 	# board = a rows by columns np.matrix that defaults with all
-	# 'X' values (representing backs of cards). 
-	@board.setter
-	def board(self, board):
-		board_vals = np.repeat('X', repeats=2 * self.n_pairs)
-		self._board = np.mat(board_vals, dtype = 'object').reshape((self.rows, self.columns))
+	# 'X' values (representing backs of cards). 	
+	# @property
+	# def board(self):
+	#  	board_vals = np.repeat('X', repeats=2 * self.n_pairs)
+	#  	board_mat = np.mat(board_vals, \
+	#  		dtype = 'object').reshape((self.rows, self.columns))
+	#  	return board_mat
 
-	@property
-	def pair_names(self):
-		return self._pair_names
-
-	# pair_names  = the words that will be underneath the X's in the 
-	# MemoryBoard.board object.  SIDE NOTE/FUN FACT: np.random.choice()
-	# is only available in numpy version >= 1.7
-	@pair_names.setter
-	def pair_names(self, pair_names):
-		self._pair_names = np.random.choice(words, size = self.n_pairs, replace = False)
+	# # pair_names  = the words that will be underneath the X's in the 
+	# # MemoryBoard.board object.  SIDE NOTE/FUN FACT: np.random.choice()
+	# # is only available in numpy version >= 1.7
+	# @pair_names.setter
+	# def pair_names(self, pair_names):
+	# 	self._pair_names = np.random.choice(words, size = self.n_pairs, replace = False)
 	
-	@property
-	def solution_dict(self):
-		return self._solution_dict
+	# @property
+	# def solution_dict(self):
+	# 	return self._solution_dict
 
-	# solution_dict: randomly allocate two copies of each item in pair_names
-	# to the total number of cards in the board.
-	@solution_dict.setter
-	def solution_dict(self, solution_dict):
-		position_list = [b for b in product([i for i in range(self.rows)], [i for i in range(self.columns)])]
-		card_names = np.repeat(self.pair_names, repeats = 2)
-		np.random.shuffle(card_names)
-		output = {}
-		for i in range(len(position_list)):
-			output[position_list[i]] = card_names[i].decode('UTF-8')
-		self._solution_dict = output
+	# # solution_dict: randomly allocate two copies of each item in pair_names
+	# # to the total number of cards in the board.
+	# @solution_dict.setter
+	# def solution_dict(self, solution_dict):
+	# 	position_list = [b for b in product([i for i in range(self.rows)], [i for i in range(self.columns)])]
+	# 	card_names = np.repeat(self.pair_names, repeats = 2)
+	# 	np.random.shuffle(card_names)
+	# 	output = {}
+	# 	for i in range(len(position_list)):
+	# 		output[position_list[i]] = card_names[i].decode('UTF-8')
+	# 	self._solution_dict = output
 	
 #Just for debugging 
 if __name__ == '__main__':
@@ -112,10 +92,10 @@ if __name__ == '__main__':
 	test1 = MemoryBoard(test_pairs)
 	print(test1.rows)
 	print(test1.columns)
-	print(test1.board) 
-	# Can i update the board in place?
-	row_col = [int(b) for b in input('Choose card to flip by index: ').split()]
-	value = test1.solution_dict[(row_col[0], row_col[1])]
-	print('Value at position (%s, %s): %s' % (row_col[0], row_col[1], value))
-	test1.board[row_col[0], row_col[1]] = value
-	print(test1.board)	
+	#print(test1.board) 
+	# # Can i update the board in place?
+	# row_col = [int(b) for b in input('Choose card to flip by index: ').split()]
+	# value = test1.solution_dict[(row_col[0], row_col[1])]
+	# print('Value at position (%s, %s): %s' % (row_col[0], row_col[1], value))
+	# test1.board[row_col[0], row_col[1]] = value
+	# print(test1.board)	
